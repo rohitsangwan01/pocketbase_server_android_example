63fc022d54168bdd5e229389b5eb16e4788cce27
Rohit Sangwan <rohitsangwan647@gmail.com>
2023-05-28 00:18:35 +0530

    implement background functionality

--- app/src/main/java/com/rohit/pocketbasandroid/MainActivity.kt
+++ app/src/main/java/com/rohit/pocketbasandroid/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.rohit.pocketbasandroid
 
+import android.Manifest.permission.POST_NOTIFICATIONS
 import android.R.attr.value
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.material.textfield.TextInputEditText
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import pocketbaseMobile.PocketbaseMobile
 import java.net.Inet4Address
 import java.net.NetworkInterface
@@ -22,26 +23,26 @@ import java.net.SocketException
 
 @SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity() {
-    private val uiScope = CoroutineScope(Dispatchers.Main + Job())
     private lateinit var txtDetails: TextView
-    var hostName = "127.0.0.1"
-    var port = "8090"
-
+    private lateinit var edtHost: TextInputEditText
+    private lateinit var edtPort: TextInputEditText
+    private lateinit var sharedPreferences: SharedPreferences
+    private val notificationPermissionResultCode = 11
+    private var hostName = Utils.defaultHostName
+    private var port = Utils.defaultPort
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         txtDetails = findViewById(R.id.textView)
+        edtHost = findViewById(R.id.edtHost)
+        edtPort = findViewById(R.id.edtPort)
         val btnRun: Button = findViewById(R.id.btnRun)
         val btnStop: Button = findViewById(R.id.btnStop)
-        val edtHost: TextInputEditText = findViewById(R.id.edtHost)
-        val edtPort: TextInputEditText = findViewById(R.id.edtPort)
         val btnAdminView: Button = findViewById(R.id.btnAdminView)
-        val sharedPreferences = getPreferences(MODE_PRIVATE)
-
+        sharedPreferences = getPreferences(MODE_PRIVATE)
         hostName = sharedPreferences.getString("hostname", getLocalIpAddress()) ?: hostName
         port = sharedPreferences.getString("port", port) ?: port
-
 
         // pass here ip address to access this in other devices under same network
         edtHost.setText(hostName)
@@ -50,28 +51,11 @@ class MainActivity : AppCompatActivity() {
         setPocketbaseCallbackListener()
 
         btnRun.setOnClickListener {
-            val dataPath: String = this.cacheDir.absolutePath
-            hostName = edtHost.text.toString()
-            port = edtPort.text.toString()
-
-            // check if empty values
-            if (hostName.isEmpty() || port.isEmpty()) {
-                txtDetails.text = "Error : hostName or port is empty"
-                return@setOnClickListener
-            }
-
-            // save to sharedPref
-            val editor = sharedPreferences.edit()
-            editor.putString("hostname", hostName)
-            editor.putString("port", port)
-            editor.apply()
-
-            // Start pocketbase server
-            startPocketbase(dataPath, hostName, port)
+            startPocketbaseService()
         }
 
         btnStop.setOnClickListener {
-            stopPocketbase()
+            stopPocketbaseService()
         }
 
         btnAdminView.setOnClickListener {
@@ -82,34 +66,99 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Register a listener to get callbacks from pocketBase and return response
     private fun setPocketbaseCallbackListener() {
         PocketbaseMobile.registerNativeBridgeCallback { command, data ->
             Log.e("Test", "PocketbaseLogs :$command : $data \n")
             this.runOnUiThread {
-                txtDetails.text = "${txtDetails.text} \nCommand: $command \nData: $data \n\n"
+                var text = "${txtDetails.text} \nCommand: $command \n"
+                if (data != null && data.isNotEmpty()) {
+                    text = "$text \nData: $data \n"
+                }
+                txtDetails.text = text
             }
             // return response back to pocketbase
             "response from native"
         }
     }
 
-    // StartPocketbase server
-    private fun startPocketbase(dataPath: String, hostname: String, port: String) {
-        uiScope.launch {
-            withContext(Dispatchers.IO) {
-                PocketbaseMobile.startPocketbase(dataPath, hostname, port)
-            }
+    private fun startPocketbaseService() {
+        if (PocketbaseService.isRunning) {
+            Toast.makeText(this, "PocketbaseService already running", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (!haveNotificationPermission()) return
+        val dataPath: String = Utils.getStoragePath(this)
+        Log.e("MainActivity", "PocketbaseDataPath : $dataPath")
+        hostName = edtHost.text.toString()
+        port = edtPort.text.toString()
+
+        // check if empty values
+        if (hostName.isEmpty() || port.isEmpty()) {
+            txtDetails.text = "Error : hostName or port is empty"
+            return
+        }
+
+        // save to sharedPref
+        val editor = sharedPreferences.edit()
+        editor.putString("hostname", hostName)
+        editor.putString("port", port)
+        editor.apply()
+
+        // Start pocketbase server
+        val intent = Intent(this, PocketbaseService::class.java)
+        intent.putExtra("dataPath", dataPath)
+        intent.putExtra("hostname", hostName)
+        intent.putExtra("port", port)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
         }
     }
 
-    // StopPocketbase server, this will close application as well (find better way to stop pocketbase)
-    private fun stopPocketbase() {
-        uiScope.launch {
-            withContext(Dispatchers.IO) {
-                PocketbaseMobile.stopPocketbase()
-            }
+    private fun stopPocketbaseService() {
+        if (!PocketbaseService.isRunning) {
+            Toast.makeText(this, "PocketbaseService not running", Toast.LENGTH_LONG).show()
+            return
         }
+        val intent = Intent(this, PocketbaseService::class.java)
+        intent.action = PocketbaseService.stopServiceAction
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+    }
+
+    private fun haveNotificationPermission(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return true
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(POST_NOTIFICATIONS),
+                notificationPermissionResultCode
+            )
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == notificationPermissionResultCode
+            && grantResults.isNotEmpty()
+            && grantResults.first() == PackageManager.PERMISSION_GRANTED
+        ) {
+            startPocketbaseService()
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     // Use localIp address to start pocketbaseServer
---
+++ app/src/main/java/com/rohit/pocketbasandroid/PocketbaseService.kt
@@ -0,0 +1,135 @@
+package com.rohit.pocketbasandroid
+
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.os.PowerManager
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import pocketbaseMobile.PocketbaseMobile
+
+class PocketbaseService : Service() {
+    private lateinit var dataPath: String
+    private var serviceWakeLock = "PocketbaseServiceWakelock"
+    private var hostname = Utils.defaultHostName
+    private var port = Utils.defaultPort
+    private val uiScope = CoroutineScope(Dispatchers.Main + Job())
+    private var wakeLock: PowerManager.WakeLock? = null
+
+    companion object {
+        const val stopServiceAction = "StopService"
+        var isRunning = false
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        dataPath = Utils.getStoragePath(this)
+        startForeground()
+        startWakelock()
+        isRunning = true
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent?.action == stopServiceAction) {
+            stopSelf()
+        } else {
+            intent?.extras?.getString("dataPath")?.let { dataPath = it }
+            intent?.extras?.getString("hostname")?.let { hostname = it }
+            intent?.extras?.getString("port")?.let { port = it }
+            startPocketbase(dataPath, hostname, port)
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForeground() {
+        val channelId = createNotificationChannel()
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+
+        // Close button Intent
+        val pendingIntentFlag =
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        val stopNotificationIntent = Intent(this, PocketbaseService::class.java)
+        stopNotificationIntent.action = stopServiceAction
+        val stopNotificationPendingIntent: PendingIntent = PendingIntent.getService(
+            this, 0,
+            stopNotificationIntent, pendingIntentFlag,
+        )
+
+        val notification = notificationBuilder
+            .setOngoing(true)
+            .setContentTitle("Pocketbase")
+            .setContentText("Pocketbase running in background")
+            .setSmallIcon(R.drawable.notification_icon)
+            .setCategory(Notification.CATEGORY_SERVICE)
+            .setPriority(NotificationManager.IMPORTANCE_HIGH)
+            .addAction(
+                R.drawable.icon_close,
+                "Stop",
+                stopNotificationPendingIntent
+            )
+            .build()
+        startForeground(124412, notification)
+    }
+
+    private fun startPocketbase(dataPath: String, hostname: String, port: String) {
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+                PocketbaseMobile.startPocketbase(dataPath, hostname, port)
+            }
+        }
+    }
+
+    private fun stopPocketbase() {
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+                PocketbaseMobile.stopPocketbase()
+            }
+        }
+    }
+
+    @SuppressLint("WakelockTimeout")
+    private fun startWakelock() {
+        val powerManager = applicationContext.getSystemService(POWER_SERVICE) as PowerManager
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, serviceWakeLock)
+        wakeLock?.acquire()
+    }
+
+    private fun stopWakelock() {
+        wakeLock?.release()
+    }
+
+    private fun createNotificationChannel(): String {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return ""
+        val chan = NotificationChannel(
+            "pocketbase_service",
+            "Pocketbase Background Service",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return "pocketbase_service"
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        stopPocketbase()
+        stopWakelock()
+        isRunning = false
+        super.onDestroy()
+    }
+}
